5fba9e7eb3cb8fa9d9ee74d4ee97d7c7a6eefab0
Harald Bregu <harald.bregu@gmail.com>
2026-09-10 00:06:49 +0200

    auto: commit after tool 2026-09-10 00:06:49

--- src/main/protocol.ts
+++ src/main/protocol.ts
@@ -293,6 +293,7 @@ export function setupMediaPermissionHandlers(appRegistry: AppRegistry): void {
 			},
 			{
 				useSystemPicker:
+					app.isPackaged &&
 					process.platform === 'darwin' &&
 					(() => {
 						try {
