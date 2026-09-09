a5557c3de5a170117b6a69ab94d8092c00239928
Harald Bregu <harald.bregu@gmail.com>
2026-09-09 22:47:28 +0200

    auto: commit after tool 2026-09-09 22:47:28

--- src/renderer/src/components/model-provider-select.tsx
+++ src/renderer/src/components/model-provider-select.tsx
@@ -117,6 +117,7 @@ export function ModelProviderSelect({
 			</PopoverTrigger>
 			<PopoverContent
 				align="end"
+				collisionPadding={12}
 				className="max-h-[var(--radix-popover-content-available-height)] w-max max-w-[calc(100vw-2rem)] overflow-y-auto p-1"
 			>
 				<div role="menu" aria-label={accessibleLabel} className="min-w-0">
