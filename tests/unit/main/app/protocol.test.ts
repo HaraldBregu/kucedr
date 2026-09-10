fa672d0ead5ef3fdf3bbf918a5205979155a4c18
Harald Bregu <harald.bregu@gmail.com>
2026-09-10 15:11:09 +0200

    auto: commit after tool 2026-09-10 15:11:09

--- tests/unit/main/app/protocol.test.ts
+++ tests/unit/main/app/protocol.test.ts
@@ -128,6 +128,19 @@ describe('protocol security', () => {
 			securityOrigin: 'file://',
 		} as Electron.MediaAccessPermissionRequest);
 		expect(legacyDisplayCaptureCallback).toHaveBeenCalledWith(true);
+		const appLegacyCaptureCallback = jest.fn();
+		request(
+			appContents as Electron.WebContents,
+			'media',
+			appLegacyCaptureCallback,
+			{
+				isMainFrame: true,
+				mediaTypes: [],
+				requestingUrl: mainUrl,
+				securityOrigin: 'file://',
+			} as Electron.MediaAccessPermissionRequest
+		);
+		expect(appLegacyCaptureCallback).toHaveBeenCalledWith(false);
 		const appCaptureCallback = jest.fn();
 		request(appContents as Electron.WebContents, 'display-capture', appCaptureCallback, {
 			isMainFrame: true,
