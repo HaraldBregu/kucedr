9848e25e116c605fa1e0c73850ad586f23f510c0
Harald Bregu <harald.bregu@gmail.com>
2026-09-11 18:40:27 +0200

    auto: commit after tool 2026-09-11 18:40:27

--- src/renderer/src/pages/settings/pages/apps/Page.tsx
+++ src/renderer/src/pages/settings/pages/apps/Page.tsx
@@ -235,7 +235,7 @@ const AppsPage: React.FC = () => {
 											<img
 												src={app.imageUrl}
 												alt=""
-												className="h-12 w-24 border border-border/70 object-cover"
+												className="size-16 border border-border/70 object-cover"
 											/>
 										</div>
 									) : (
