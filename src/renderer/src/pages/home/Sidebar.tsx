82942da75e0a8d475a356bb95f9133b762d9a158
Harald Bregu <harald.bregu@gmail.com>
2026-09-15 12:35:57 +0200

    auto: commit after tool 2026-09-15 12:35:57

--- src/renderer/src/pages/home/Sidebar.tsx
+++ src/renderer/src/pages/home/Sidebar.tsx
@@ -265,7 +265,7 @@ export function HomeSidebar({ refreshKey }: HomeSidebarProps): ReactElement {
 								className="w-[var(--radix-dropdown-menu-trigger-width)] min-w-56 rounded-lg"
 								side="top"
 								align="end"
-								alignOffset={8}
+								alignOffset={16}
 							>
 								<DropdownMenuLabel className="p-0 font-normal">
 									<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
