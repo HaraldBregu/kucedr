b154fc55dce98365e334c1170ccf432b72727435
Harald Bregu <harald.bregu@gmail.com>
2026-09-15 12:46:13 +0200

    auto: commit after tool 2026-09-15 12:46:13

--- src/renderer/src/pages/home/Sidebar.tsx
+++ src/renderer/src/pages/home/Sidebar.tsx
@@ -272,7 +272,7 @@ export function HomeSidebar({ refreshKey }: HomeSidebarProps): ReactElement {
 							<DropdownMenuContent
 								className="w-52 rounded-lg"
 								side="top"
-								align="end"
+								align="center"
 							>
 								<DropdownMenuLabel className="p-0 font-normal">
 									<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
