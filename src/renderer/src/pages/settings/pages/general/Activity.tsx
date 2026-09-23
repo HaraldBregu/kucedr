f296a43eb409728c3110abd4eecc13aaac787e60
Harald Bregu <harald.bregu@gmail.com>
2026-09-23 23:34:22 +0200

    auto: commit after tool 2026-09-23 23:34:22

--- src/renderer/src/pages/settings/pages/general/Activity.tsx
+++ src/renderer/src/pages/settings/pages/general/Activity.tsx
@@ -47,7 +47,7 @@ export function Activity(): React.JSX.Element {
 			>
 				<CalendarHeatmap
 					values={[...values]}
-					weeks={40}
+					weeks={42}
 					gap={2}
 					cellSize={12}
 					weekStart={1}
