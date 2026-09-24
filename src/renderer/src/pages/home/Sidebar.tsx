7648f2061e438724ce122eebe30836b7d85ee368
Harald Bregu <harald.bregu@gmail.com>
2026-09-24 21:40:07 +0200

    auto: commit after tool 2026-09-24 21:40:07

--- src/renderer/src/pages/home/Sidebar.tsx
+++ src/renderer/src/pages/home/Sidebar.tsx
@@ -339,7 +339,7 @@ export function HomeSidebar({ refreshKey }: HomeSidebarProps): ReactElement {
 								</SidebarMenuButton>
 							</DropdownMenuTrigger>
 							<DropdownMenuContent
-								className="w-52 rounded-lg"
+								className="w-64 rounded-lg"
 								side="top"
 								align="start"
 							>
